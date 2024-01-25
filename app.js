363a4fb0a8e2f28bd7f0d1736cc71fb8bf9b41a7
Harry <152967791+0Harrison@users.noreply.github.com>
2024-01-25 23:31:44 +0100

    function 2

---
+++ app.js
@@ -0,0 +1,8 @@
+//Create a function that returns either true or false if the age parameter is above "30".
+
+function isAbove30(age){
+    return age > 90;
+}
+
+let result = isAbove30(35);
+console.log(result);
